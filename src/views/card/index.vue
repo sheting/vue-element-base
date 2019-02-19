da578ae28c348d0abc456b4cbac59ab162e01984
crystal <yanghuanfang@chainnova.com>
2019-02-19 17:03:47 +0800

    card content

--- src/views/card/index.vue
+++ src/views/card/index.vue
@@ -1,5 +1,29 @@
 <template lang="pug">
-  p card
+el-row(:gutter="20")
+  el-col(:span="8")
+    h2 带阴影的card
+    el-card.box-card(shadow="always")
+      div.clearfix(slot="header")
+        span 北大荒
+        el-button.button( type='text') 操作按钮
+      div.item(v-for="n in  4" :key="n")
+        | 列表内容{{n}}
+  el-col(:span="8")
+    h2 悬浮出现阴影
+    el-card.box-card(shadow="hover")
+      div.clearfix(slot="header")
+        span 北大荒
+        el-button.button( type='text') 操作按钮
+      div.item(v-for="n in  4" :key="n")
+        | 列表内容{{n}}
+  el-col(:span="8")
+    h2 不带阴影的card
+    el-card.box-card(shadow="never")
+      div.clearfix(slot="header")
+        span 北大荒
+        el-button.button(type='text') 操作按钮
+      div.item(v-for="n in  4" :key="n")
+        | 列表内容{{n}}
 </template>
 
 <script>
@@ -13,4 +37,18 @@ export default {
 }
 </script>
 <style scoped lang="postcss">
+h2 {
+  line-height: 50px;
+  font-weight: bold;
+}
+.item{
+  margin-bottom: 15px;
+}
+.box-card{
+  max-width: 500px;
+}
+.button{
+  float: right;
+  padding:3px 0;
+}
 </style>
