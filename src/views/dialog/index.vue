1260fdca93d6fa7c08d097396706206c17315ae2
crystal <yanghuanfang@chainnova.com>
2019-02-20 17:24:24 +0800

    confirm dialog

--- src/views/dialog/index.vue
+++ src/views/dialog/index.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   el-card
     el-button(@click="dialogVisible = true") 点击打开dialog
+    el-button(@click="confirmVisible = true") 点击打开confirm
     el-dialog(
       :visible.sync="dialogVisible"
       width="40%"
@@ -11,7 +12,17 @@
         p 这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动这是一段文字描述用来测试内容超出滚动
       span.dialog-footer(slot="footer")
         el-button(type="primary" @click="dialogVisible = false") 确定 
-        el-button(type="info" plain @click="dialogVisible = false") 取消 
+        el-button(type="info" plain @click="dialogVisible = false") 取消
+    el-dialog(
+      title="提示"
+      :visible.sync="confirmVisible"
+      width="350px"
+      append-to-body)
+      div
+        p 确定要删除该阶段吗？
+      span.dialog-footer(slot="footer")
+        el-button(type="primary" @click="confirmVisible = false") 确定 
+        el-button(type="info" plain @click="confirmVisible = false") 取消 
 </template>
 
 <script>
@@ -20,6 +31,7 @@ export default {
   data() {
     return {
       dialogVisible: false,
+      confirmVisible: false,
       title: "审核申请"
     };
   },
