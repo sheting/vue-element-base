3ce3cac3abab89a14cef6d6d1de650c41035f4ae
crystal <yanghuanfang@chainnova.com>
2019-02-20 16:40:58 +0800

    dialog

--- src/router.js
+++ src/router.js
@@ -1,84 +1,94 @@
 const routers = [
-    {
-      path: '/',
-      name: 'demo',
-      meta: {
-        title: '首页',
-        scrollToTop: true,
-        // requireAuth: true
-      },
-      component: () => import('@/views/layout'),
-      redirect: '/dashboard',
-      children: [
-        {
-          path: 'dashboard',
-          component: () => import('@/views/dashboard'),
-          name: 'dashboard',
-          meta: {
-            title: 'dashboard',
-            // requireAuth: true,
-            noCache: true
-          }
-        },
-        {
-          path: 'components',
-          component: () => import('@/views/base-layout'),
-          name: 'components',
-          redirect: '/components/table',
-          meta: {
-            title: 'components',
-            // requireAuth: true,
-            noCache: true
-          },
-          children: [
-            {
-              path: 'table',
-              component: () => import('@/views/table'),
-              name: 'table',
-              meta: {
-                title: 'tables',
-                // requireAuth: true,
-                noCache: true
-              }
-            },
-            {
-              path: 'card',
-              component: () => import('@/views/card'),
-              name: 'card',
-              meta: {
-                title: 'cards',
-                // requireAuth: true,
-                noCache: true
-              }
-            },
-            {
-              path: 'loading',
-              component: () => import('@/views/loading'),
-              name: 'loading',
-              meta: {
-                title: 'loading',
-                // requireAuth: true,
-                noCache: true
-              }
-            }
-          ]
+  {
+    path: '/',
+    name: 'demo',
+    meta: {
+      title: '首页',
+      scrollToTop: true,
+      // requireAuth: true
+    },
+    component: () => import('@/views/layout'),
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/dashboard'),
+        name: 'dashboard',
+        meta: {
+          title: 'dashboard',
+          // requireAuth: true,
+          noCache: true
         }
-      ]
-    },
-    {
-      path: '/login',
-      alias: '/signin',
-      name: 'login',
-      component: () => import('@/views/login'),
-      meta: {title: `登录`}
-    },
-    {
-      path: '/404',
-      alias: '/not-found',
-      name: '404',
-      component: () => import('@/views/404'),
-      meta: {title: `Error - 404`}
-    }
-  ]
-  
-  export default routers
+      },
+      {
+        path: 'components',
+        component: () => import('@/views/base-layout'),
+        name: 'components',
+        redirect: '/components/table',
+        meta: {
+          title: 'components',
+          // requireAuth: true,
+          noCache: true
+        },
+        children: [
+          {
+            path: 'table',
+            component: () => import('@/views/table'),
+            name: 'table',
+            meta: {
+              title: 'tables',
+              // requireAuth: true,
+              noCache: true
+            }
+          },
+          {
+            path: 'card',
+            component: () => import('@/views/card'),
+            name: 'card',
+            meta: {
+              title: 'cards',
+              // requireAuth: true,
+              noCache: true
+            }
+          },
+          {
+            path: 'loading',
+            component: () => import('@/views/loading'),
+            name: 'loading',
+            meta: {
+              title: 'loading',
+              // requireAuth: true,
+              noCache: true
+            }
+          },
+          {
+            path: 'dialog',
+            component: () => import('@/views/dialog'),
+            name: 'dialog',
+            meta: {
+              title: 'dialog',
+              // requireAuth: true,
+              noCache: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/login',
+    alias: '/signin',
+    name: 'login',
+    component: () => import('@/views/login'),
+    meta: { title: `登录` }
+  },
+  {
+    path: '/404',
+    alias: '/not-found',
+    name: '404',
+    component: () => import('@/views/404'),
+    meta: { title: `Error - 404` }
+  }
+]
+
+export default routers
---
+++ src/views/dialog/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  el-card
+    el-button(@click="dialogVisible = true") 点击打开dialog
+</template>
+
+<script>
+export default {
+  name: "dialog",
+  data() {
+    return {
+      dialogVisible: false
+    };
+  },
+  methods: {
+    // openFullScreen() {
+    //   this.loading1 = true;
+    //   setTimeout(() => {
+    //     this.loading1 = false;
+    //   }, 2000);
+    // },
+    // openLoading() {
+    //   this.loading2 = true;
+    //   setTimeout(() => {
+    //     this.loading2 = false;
+    //     this.nums = [0, 1, 2, 3, 4, 5];
+    //   }, 2000);
+    // }
+  }
+};
+</script>
+<style scoped lang="postcss">
+</style>
