7c458b6b4fd7db793ebfbb5f3fc2e94e89175a3d
crystal <yanghuanfang@chainnova.com>
2019-02-20 14:07:35 +0800

    css

--- src/views/card/index.vue
+++ src/views/card/index.vue
@@ -111,13 +111,13 @@ h2 {
   & .content {
     height: 250px;
   }
-  [class^="svg-icon"] {
-    vertical-align: text-bottom;
-    margin-right: 5px;
-    width: 20px;
-    text-align: center;
-    font-size: 18px;
-    color: #909399;
-  }
+}
+[class^="svg-icon"] {
+  vertical-align: text-bottom;
+  margin-right: 5px;
+  width: 20px;
+  text-align: center;
+  font-size: 18px;
+  color: #909399;
 }
 </style>
