0d966fe66d5c1c9a00c35bb9aa5c8cf2d13c58cc
xuting <xuting@chainnova.com>
2019-02-20 11:35:18 +0800

    404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,16 @@
 <template lang="pug">
-  p 404
+  el-row
+    el-col(:span="24")
+      .page-not-found
+        .title
+          svg-icon(icon-class="404-page", class-name="not-found-icon")
+          .title-desc
+            .desc-404 404
+            .desc-not-found Page Not found
+        .content
+          | Oops, The page you're looking for cannot be found.
+        .footer
+          router-link(to="/dashboard") 返回首页
 </template>
 
 <script>
@@ -13,4 +24,18 @@ export default {
 }
 </script>
 <style scoped lang="postcss">
+.page-not-found{
+  width: 330px;margin: 0 auto;margin-top: 60px;
+  & .title{
+    display: flex;justify-content: center;align-items: center;
+  }
+  & .title-desc{margin-left: 20px;}
+  & .desc-404{font-size: 24px;font-weight: 500;}
+  & .content{font-size: 14px;text-align: center;margin-top: 15px;}
+  & .footer{
+    margin-top: 15px;padding:8px;border-top: 1px solid #e9e9e9;text-align: center;
+    & > a{text-decoration: none;color: #36a3f7;}
+  }
+}
+.not-found-icon{font-size: 98px;color: #36a3f7;}
 </style>
