a515d81587720ee80d1df8ed63446f2e94ea178c
xuting <xuting@chainnova.com>
2019-03-07 10:43:02 +0800

    chart layout

--- src/views/chart/index.vue
+++ src/views/chart/index.vue
@@ -1,7 +1,26 @@
 <template lang="pug">
-el-row(:gutter="20")
-  el-col(:span="6")
-    | test
+el-card
+  el-row(:gutter="20")
+    el-col.chart-item(:span="8")
+      el-card
+        div(slot="header")
+          span 基础折线图
+        div test
+    el-col.chart-item(:span="8")
+      el-card
+        div(slot="header")
+          span 双折线图
+        div test
+    el-col.chart-item(:span="8")
+      el-card
+        div(slot="header")
+          span 基础柱状图
+        div test
+    el-col.chart-item(:span="8")
+      el-card
+        div(slot="header")
+          span 堆叠柱状图
+        div test
 </template>
 
 <script lang="ts">
@@ -15,3 +34,5 @@ export default class ChartComponent extends Vue {
 }
 </script>
 <style scoped lang="postcss">
+.chart-item{background-color: #fff;margin-bottom: 20px;}
+</style>
