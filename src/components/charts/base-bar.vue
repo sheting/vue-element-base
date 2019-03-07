f9880cab6bb28bea6d08d034925debc8a1db13fb
xuting <xuting@chainnova.com>
2019-03-07 18:41:48 +0800

    base bar config

---
+++ src/components/charts/base-bar.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+#base-bar
+</template>
+
+<script lang="ts">
+import { Component, Prop, Watch, Vue } from 'vue-property-decorator'
+import G2 from '@antv/g2'
+@Component({
+  name: 'base-bar',
+  components: {}
+})
+export default class BaseBarComponent extends Vue {
+  // prop data
+  @Prop() readonly data!: object[]
+  chart: any
+  // method
+  initChart () {
+    this.chart = new G2.Chart({
+      container: 'base-bar',
+      forceFit: true,
+      height: 400,
+      padding: [20, 20, 30, 90],
+      animate: true
+    })
+  }
+  paintChart () {
+    this.chart.source(this.data, {
+      value: {
+        tickCount: 5
+      }
+    })
+    this.chart.scale('value', {
+      alias: '销售额(万)'
+    })
+    this.chart.axis('value', {
+      label: {
+        textStyle: {
+          fill: '#aaaaaa'
+        },
+        formatter: function formatter(text: any) {
+          return text.replace(/(\d)(?=(\d{3})+\.)/g, '$1,')
+        }
+      },
+      title: {
+        offset: 70
+      }
+    })
+    // bar 相关配置
+    this.chart.interval().position('type*value').opacity(1)
+    this.chart.render()
+  }
+  updateChart () {
+    this.chart.changeData(this.data)
+  }
+  // watch
+  @Watch('data')
+  onChangeData (nv: object[], ov: object[]) {
+    if (nv) this.updateChart()
+  }
+  // hook
+  mounted () {
+    this.initChart()
+    this.paintChart()
+  }
+}
+</script>
+<style scoped lang="postcss">
+</style>
--- src/views/chart/index.vue
+++ src/views/chart/index.vue
@@ -17,7 +17,7 @@ el-card
       el-card
         div(slot="header")
           span 基础柱状图
-        div test
+        base-bar(:data="baseBarData")
     el-col.chart-item(:xs="24" :sm="24" :md="12" :lg="12" :xl="8")
       el-card
         div(slot="header")
@@ -28,13 +28,16 @@ el-card
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import baseLine from '@/components/charts/base-line.vue'
+import baseBar from '@/components/charts/base-bar.vue'
 
 @Component({
   name: 'chart-component',
-  components: {baseLine}
+  components: {baseLine, baseBar}
 })
 export default class ChartComponent extends Vue {
   private baseLineData: object[] = []
+  private baseBarData: object[] = []
+
   updateBaseLine () {
     const mockBaseLine = []
     for (let i = 0; i < 6; i++) {
@@ -46,8 +49,21 @@ export default class ChartComponent extends Vue {
     }
     this.baseLineData = mockBaseLine.slice(0)
   }
+  updateBaseBar () {
+    const mockBaseBar = []
+    const types = [`家具家电`, `粮油副食`, `生鲜水果`, `美容洗护`, `母婴用品`, `家庭清洁`]
+    for (let i = 0; i < 6; i++) {
+      const obj = {
+        type: types[i],
+        value: Math.floor(Math.random() * 100000)
+      }
+      mockBaseBar.push(obj)
+    }
+    this.baseBarData = mockBaseBar.slice(0)
+  }
   mounted () {
     this.updateBaseLine()
+    this.updateBaseBar()
   }
 }
 </script>
