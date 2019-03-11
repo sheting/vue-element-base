e4f88212cc5f6e984443c18e4c03ef8eef68291c
xuting <xuting@chainnova.com>
2019-03-11 18:01:06 +0800

    add webpack analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   outputDir: 'dist',
@@ -18,14 +17,19 @@ module.exports = {
         devtool: 'source-map'
       }
     } else {
-      return {
-        plugins: [
-          new BundleAnalyzerPlugin()
-        ]
-      }
+      return {}
     }
   },
   chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      if (process.env.npm_config_report) {
+        config
+          .plugin('webpack-bundle-analyzer')
+          .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+          .end();
+      }
+    } else {
+    }
     const svgRule = config.module.rule('svg')
     // 清除已有的所有 loader。
     // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后。
